3c351fcb8354169d496eaf5401dfaf412984ccd2
Syed Ahammad Basha <syed2005ahamed@gmail.com>
2025-01-21 22:14:03 +0530

    Add files via upload

---
+++ stud.py
@@ -0,0 +1,19 @@
+Number = int(input('Student Number :'))
+Name   = input('Enter a Name :')
+c      = int(input('Enter c marks :'))
+cpp      = int(input('Enter cpp marks :'))
+java      = int(input('Enter java marks :'))
+python      = int(input('Enter python marks :'))
+
+tot = c+cpp+java+python;
+avg = tot/4.0;
+
+print('Student Number  :',Number)
+print('Student Name    :',Name)
+print(' C marks        :',c)
+print(' CPP marks      :',cpp)
+print(' java marks     :',java)
+print(' python marks   :',python)
+print(' Total marks    :',tot)
+print(' Average marks  :',avg)
+print('Incharge')
